eae0df234db083c2367230646f4562104f9da033
Eric Henderson <henderea@gmail.com>
2020-10-31 10:59:26 -0400

    add an inverse mode

--- lib/model.ts
+++ lib/model.ts
@@ -1,7 +1,7 @@
 // @ts-ignore
 import { ko, obs, obsArr, comp } from 'lib/knockout-util.ts';
 
-import { TYPE_LIST, PokemonTypeName, PokemonType, AttackMultiplier, convertAllTypes, extractAllTypes, convertType } from 'lib/pokemon-types';
+import { TYPE_LIST, PokemonTypeName, PokemonType, AttackMultiplier, convertAllTypes, extractAllTypes, convertType, SingleTypeAttackMultiplier } from 'lib/pokemon-types';
 
 function multiplierToClass(multiplier: AttackMultiplier): string {
   switch(multiplier) {
@@ -26,29 +26,31 @@ function classNameComp(self: Model, type: PokemonType): ko.PureComputed<string> 
   return comp(self, (self: Model): string => {
     const activeTypes = self.activeTypes();
     if(activeTypes.length <= 0) { return `${type.typeName} x1`; }
+    if(self.inverse) {
+      const multiplier: SingleTypeAttackMultiplier = type.defense(activeTypes[0]);
+      return `${type.typeName} ${multiplierToClass(multiplier)}`;
+    }
     const multiplier: AttackMultiplier = type.attack(activeTypes[0], activeTypes[1] || null);
     return `${type.typeName} ${multiplierToClass(multiplier)}`;
   });
 }
 
 const dbgOrder: PokemonType[] = convertAllTypes('grass', 'water', 'fire', 'ice');
+const invOrder: PokemonType[] = convertAllTypes('dark', 'psychic', 'fighting');
 
 class Model {
-  readonly activeTypes: ko.ObservableArray<PokemonType>;
-  private readonly _showDebug: ko.Observable<boolean>;
-  private readonly _iconSize: ko.Observable<number>;
-  private readonly _windowWidth: ko.Observable<number>;
-  private readonly _windowHeight: ko.Observable<number>;
+  readonly activeTypes: ko.ObservableArray<PokemonType> = obsArr();
+  private readonly _showDebug: ko.Observable<boolean> = obs(false);
+  private readonly _iconSize: ko.Observable<number> = obs(-1);
+  private readonly _windowWidth: ko.Observable<number> = obs(-1);
+  private readonly _windowHeight: ko.Observable<number> = obs(-1);
   private readonly _extraInfo: ko.PureComputed<string>;
   readonly classNames: { [key in PokemonTypeName]: ko.PureComputed<string> };
   private _debugToggleCounter: number = 0;
+  private _inverseToggleCounter: number = 0;
+  private readonly _inverse: ko.Observable<boolean> = obs(false);
 
   constructor() {
-    this.activeTypes = obsArr();
-    this._showDebug = obs(false);
-    this._iconSize = obs(-1);
-    this._windowWidth = obs(-1);
-    this._windowHeight = obs(-1);
     this._extraInfo = comp(this, (self: Model) => {
       if(!self.showDebug) { return ''; }
       // @ts-ignore
@@ -84,7 +86,13 @@ class Model {
 
   get extraInfo(): string { return this._extraInfo(); }
 
+  get inverse(): boolean { return this._inverse(); }
+
+  set inverse(value: boolean) { this._inverse(value); }
+
   loadFromStorage(): void {
+    const inverse: Optional<string> = window.localStorage.getItem('inverse');
+    this.inverse = !!inverse && inverse == 't';
     const activeTypes: Optional<string> = window.localStorage.getItem('activeTypes');
     if(activeTypes) {
       this.activeTypes.removeAll();
@@ -102,31 +110,25 @@ class Model {
     } else {
       window.localStorage.setItem('activeTypes', extractAllTypes(...activeTypes).join('|'));
     }
+    window.localStorage.setItem('inverse', this.inverse ? 't' : 'f');
   }
 
   toggleActiveType(typeName: PokemonTypeName): void {
-    // if(this._debugToggleLast == typeName) {
-    //   this._debugToggleCount++;
-    //   if(this._debugToggleCount >= 4) {
-    //     this._debugToggleLast = null;
-    //     this._debugToggleCount = 0;
-    //     this.showDebug = !this.showDebug;
-    //   }
-    // } else {
-    //   this._debugToggleLast = typeName;
-    //   this._debugToggleCount = 1;
-    // }
     const type: PokemonType = convertType(typeName);
     let dbgInc: boolean = false;
+    let invInc: boolean = false;
     if(this.activeTypes().includes(type)) {
       this.activeTypes.remove(type);
     } else {
-      while(this.activeTypes().length > 1) {
+      while(this.activeTypes().length > (this.inverse ? 0 : 1)) {
         this.activeTypes.shift();
       }
       this.activeTypes.push(type);
       if(dbgOrder[this._debugToggleCounter] == type) {
         dbgInc = true;
+      }
+      if(invOrder[this._inverseToggleCounter] == type) {
+        invInc = true;
       }
     }
     if(dbgInc) {
@@ -137,6 +139,18 @@ class Model {
       }
     } else {
       this._debugToggleCounter = 0;
+    }
+    if(invInc) {
+      this._inverseToggleCounter++;
+      if(this._inverseToggleCounter >= invOrder.length) {
+        this._inverseToggleCounter = 0;
+        this.inverse = !this.inverse;
+        if(this.inverse && this.activeTypes().length > 1) {
+          this.activeTypes.shift();
+        }
+      }
+    } else {
+      this._inverseToggleCounter = 0;
     }
     this.saveToStorage();
   }
